e3ab1bdb388b55d34269dfbe49e42e9b506c74fd
JakeSmokie <jakesmokie@gmail.com>
2018-05-26 14:29:31 +0300

    refactored code

--- ZandronumServersDataCollector/Program.cs
+++ ZandronumServersDataCollector/Program.cs
@@ -1,10 +1,10 @@
-﻿using System.Net;
+﻿using System.Linq;
 
 namespace ZandronumServersDataCollector {
     internal class Program {
         private static void Main(string[] args) {
             var fetcher = new ServerListFetcher();
-            var servers = fetcher.FetchServerList(("master.zandronum.com", 15300));
+            var servers = fetcher.FetchServerList(("master.zandronum.com", 15300)).ToList();
 
 
         }
--- ZandronumServersDataCollector/ServerListFetcher.cs
+++ ZandronumServersDataCollector/ServerListFetcher.cs
@@ -4,24 +4,17 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Security.Cryptography;
-using System.Threading;
 using EncodeLibrary.Huffman;
 
 namespace ZandronumServersDataCollector {
     public class ServerListFetcher {
         private enum ResponseReadStates {
             WaitForNextServerListPart,
-
-            End,
-            ReadServerBlock
+            ReadServerBlock,
+            End
         }
 
-        private readonly HuffmanCodec _huffmanCodec;
-
-        public ServerListFetcher() {
-            _huffmanCodec = new HuffmanCodec(HuffmanCodec.SkulltagCompatibleHuffmanTree);
-        }
+        private static readonly HuffmanCodec HuffmanCodec = new HuffmanCodec(HuffmanCodec.SkulltagCompatibleHuffmanTree);
 
         public IEnumerable<IPEndPoint> FetchServerList(IEnumerable<(string host, int port)> masterServers) {
             var serverList = new List<IPEndPoint>();
@@ -34,80 +27,71 @@ namespace ZandronumServersDataCollector {
         }
 
         public IEnumerable<IPEndPoint> FetchServerList((string host, int port) masterServer) {
-            var query = ConstructLauncherQuery();
-
             using (var udpClient = new UdpClient()) {
-                udpClient.Connect(masterServer.host, masterServer.port);
-
-                var sendTask = udpClient.SendAsync(query, query.Length);
-                sendTask.Wait();
-
-                IPEndPoint ip = null;
-
-                var recievedData = new List<byte>();
-
-                do {
-                    var buffer = udpClient.Receive(ref ip);
-                    recievedData.AddRange(buffer);
-                } while (udpClient.Available != 0);
+                ConnectAndSendQuery(masterServer, udpClient);
+                var recievedData = ReceivePlainData(udpClient);
 
                 // check for unencoded data
                 var serverResponse =
                     recievedData[0] == 0xFF
                         ? recievedData.Skip(1).ToArray()
-                        : _huffmanCodec.Decode(recievedData.ToArray());
+                        : HuffmanCodec.Decode(recievedData.ToArray());
 
                 return ReadResponse(new BinaryReader(new MemoryStream(serverResponse)));
             }
         }
 
+        private static void ConnectAndSendQuery((string host, int port) masterServer, UdpClient udpClient)
+        {
+            udpClient.Connect(masterServer.host, masterServer.port);
+            var query = ConstructLauncherQuery();
+
+            var sendTask = udpClient.SendAsync(query, query.Length);
+            sendTask.Wait();
+        }
+
+        private static byte[] ConstructLauncherQuery() {
+            var message = new List<byte>();
+
+            message.AddRange(BitConverter.GetBytes((int) MasterServerCommands.LauncherMasterChallenge));
+            message.AddRange(BitConverter.GetBytes((short) MasterServerVersion.UsualVersion));
+
+            return HuffmanCodec.Encode(message.ToArray());
+        }
+
+        private static List<byte> ReceivePlainData(UdpClient udpClient)
+        {
+            var recievedData = new List<byte>();
+
+            do {
+                IPEndPoint ip = null;
+                var buffer = udpClient.Receive(ref ip);
+
+                recievedData.AddRange(buffer);
+            } while (udpClient.Available != 0);
+
+            return recievedData;
+        }
+
         private static IEnumerable<IPEndPoint> ReadResponse(BinaryReader serverResponse) {
             var state = ResponseReadStates.WaitForNextServerListPart;
-            var packetNum = 0;
 
             while (state != ResponseReadStates.End) {
                 switch (state) {
                     case ResponseReadStates.WaitForNextServerListPart:
-                        // check for unencoded data
-                        if (serverResponse.ReadByte() != 0xFF) {
-                            serverResponse.BaseStream.Seek(-1, SeekOrigin.Current);
-                        }
-
-                        if (serverResponse.ReadInt32() == (int) MasterResponseCommands.BeginServerListPart) {
-                            packetNum = serverResponse.ReadByte();
-                            state = ResponseReadStates.ReadServerBlock;
-
-                            if (serverResponse.ReadByte() != (int) MasterResponseCommands.ServerBlock) {
-                                throw new ServerListFetcherException("");
-                            }
-                        }
+                        WaitForNextServerListPart(serverResponse);
+                        state = ResponseReadStates.ReadServerBlock;
 
                         break;
                     case ResponseReadStates.ReadServerBlock:
                         // is server block terminated
                         if (serverResponse.ReadByte() == 0) {
-                            var responseCommand = serverResponse.ReadByte();
-
-                            if (responseCommand == (int) MasterResponseCommands.EndServerList) {
-                                state = ResponseReadStates.End;
-                            }
-
-                            if (responseCommand == (int) MasterResponseCommands.EndServerListPart) {
-                                state = ResponseReadStates.WaitForNextServerListPart;
-                            }
-
+                            state = FindOutIsItEndOfServerList(serverResponse);
                             break;
                         }
 
-                        // we read 1 byte so we need to seek 1 byte backward
-                        serverResponse.BaseStream.Seek(-1, SeekOrigin.Current);
-
-                        var portsAmount = serverResponse.ReadByte();
-                        var ip = serverResponse.ReadUInt32();
-
-                        for (var i = 0; i < portsAmount; i++) {
-                            yield return new IPEndPoint(ip, serverResponse.ReadUInt16());
-                        }
+                        foreach (var ipEndPoint in ReadServers(serverResponse))
+                            yield return ipEndPoint;
 
                         break;
                     case ResponseReadStates.End:
@@ -118,13 +102,50 @@ namespace ZandronumServersDataCollector {
             }
         }
 
-        private byte[] ConstructLauncherQuery() {
-            var message = new List<byte>();
+        private static IEnumerable<IPEndPoint> ReadServers(BinaryReader serverResponse)
+        {
+            // we read 1 byte so we need to seek 1 byte backward
+            serverResponse.BaseStream.Seek(-1, SeekOrigin.Current);
 
-            message.AddRange(BitConverter.GetBytes((int) MasterServerCommands.LauncherMasterChallenge));
-            message.AddRange(BitConverter.GetBytes((short) MasterServerVersion.UsualVersion));
+            var portsAmount = serverResponse.ReadByte();
+            var ip = serverResponse.ReadUInt32();
 
-            return _huffmanCodec.Encode(message.ToArray());
+            for (var i = 0; i < portsAmount; i++) {
+                yield return new IPEndPoint(ip, serverResponse.ReadUInt16());
+            }
+        }
+
+        private static ResponseReadStates FindOutIsItEndOfServerList(BinaryReader serverResponse)
+        {
+            var responseCommand = (MasterResponseCommands) serverResponse.ReadByte();
+
+            switch (responseCommand) {
+                case MasterResponseCommands.EndServerList:
+                    return ResponseReadStates.End;
+                case MasterResponseCommands.EndServerListPart:
+                    return ResponseReadStates.WaitForNextServerListPart;
+                default:
+                    throw new ServerListFetcherException("Incorrect response");
+            }
+        }
+
+        private static void WaitForNextServerListPart(BinaryReader serverResponse)
+        {
+            // check for unencoded data
+            if (serverResponse.ReadByte() != 0xFF) {
+                serverResponse.BaseStream.Seek(-1, SeekOrigin.Current);
+            }
+
+            if (serverResponse.ReadInt32() != (int) MasterResponseCommands.BeginServerListPart) {
+                throw new ServerListFetcherException("Incorrect response");
+            }
+
+            // read unused packet number
+            serverResponse.ReadByte(); 
+
+            if (serverResponse.ReadByte() != (int) MasterResponseCommands.ServerBlock) {
+                throw new ServerListFetcherException("Incorrect response");
+            }
         }
     }
 }
