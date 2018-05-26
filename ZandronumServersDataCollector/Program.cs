24edbe6882cb3ecabcfe8fc0dac1eced3065c06c
JakeSmokie <jakesmokie@gmail.com>
2018-05-26 14:05:24 +0300

    removed unneed master server

--- ZandronumServersDataCollector/Program.cs
+++ ZandronumServersDataCollector/Program.cs
@@ -4,10 +4,9 @@ namespace ZandronumServersDataCollector {
     internal class Program {
         private static void Main(string[] args) {
             var fetcher = new ServerListFetcher();
-            var servers = fetcher.FetchServerList(new[] {
-                ("master.zandronum.com", 15300),
-                ("localhost", 15300)
-            });
+            var servers = fetcher.FetchServerList(("master.zandronum.com", 15300));
+
+
         }
     }
 }
